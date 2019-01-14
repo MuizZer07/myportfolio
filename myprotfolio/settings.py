c1ef8b564a4a3c349aac2c6b368c1856d3c8dc61
MuizZer07 <muizxzer07@gmail.com>
2019-01-15 02:49:25 +0600

    static error fix

--- myprotfolio/settings.py
+++ myprotfolio/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'myprotfolio/static/')
+    os.path.join(BASE_DIR, 'myprotfolio/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')

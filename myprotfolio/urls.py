3c5162ff39bbb825bbf7767980115f4e9599f6fd
MuizZer07 <muizxzer07@gmail.com>
2019-01-15 02:36:43 +0600

    static error

--- myprotfolio/urls.py
+++ myprotfolio/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+import web.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', web.views.home, name='home')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
